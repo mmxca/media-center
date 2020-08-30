31f028c50cbd636a77fa1f720bcba58e8a60287b
David Hoffman <david.hoffman@hitachivantara.com>
2020-08-30 18:02:40 -0500

    Updates

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-envsubst "`env | awk -F = '{printf \" \\\\$%s\", $1}'`" < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && \
+  envsubst "`env | awk -F = '{printf \" \\\\$%s\", $1}'`" < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf && \
 
-echo "# NS1 API credentials used by Certbot" > /etc/.ns1creds && \
-echo "dns_nsone_api_key = $LETS_ENCRYPT_API_KEY" >> /etc/.ns1creds && \
-chmod 600 /etc/.ns1creds && \
+  echo "# NS1 API credentials used by Certbot" > /etc/.ns1creds && \
+  echo "dns_nsone_api_key = $LETS_ENCRYPT_API_KEY" >> /etc/.ns1creds && \
+  chmod 600 /etc/.ns1creds \
 
-#certbot certonly --nginx --dns-nsone --dns-nsone-credentials /etc/.ns1creds --dns-nsone-propagation-seconds 60 -d mom.mmxca.com -m david@mmxca.com --accept && \
+  certbot certonly --dns-nsone --dns-nsone-credentials /etc/.ns1creds --dns-nsone-propagation-seconds 60 -d $NGINX_FQDN -m $NGINX_ADMIN_EMAIL --agree-tos -n && \
 
-nginx -g 'daemon off;'
+# echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+
+  nginx -g 'daemon off;'
